6c55aa5a92da1104c279d135a44b03af2c0da608
pekshevds <pekshev.ds@gmail.com>
2022-10-01 17:30:21 +0300

    fix bags in sporages and creating points

--- flaskr/samples.py
+++ flaskr/samples.py
@@ -1,11 +1,11 @@
-from flaskr.models import Storage
+from flaskr.models import Storage, Point
 
 
 def get_storages(storage_kind=None) -> list:
     if storage_kind:
-        return Storage.query.filter_by(kind=storage_kind).all()
+        return Storage.query.filter_by(kind=storage_kind, is_mark=False).all()
     else:
-        return Storage.query.all()
+        return Storage.query.filter_by(is_mark=False).all()
     
 
 def get_storage_by_id(id: int) -> Storage:
@@ -14,3 +14,15 @@ def get_storage_by_id(id: int) -> Storage:
     except:
         storage = None    
     return storage
+
+
+def get_points() -> list:
+    return Point.query.filter_by(is_mark=False).all()
+
+
+def get_point_by_id(id: int) -> Point:
+    try:
+        point = Point.query.get(id)
+    except:
+        point = None    
+    return point
--- flaskr/views.py
+++ flaskr/views.py
@@ -1,7 +1,4 @@
-from xmlrpc.client import boolean
-from flask import render_template, redirect, url_for
-from flaskr.samples import get_storages, get_storage_by_id
-from flaskr.changes import delete_storage_by_id, add_storage, update_storage
+from flask import render_template
 
 
 def get_home_view():
@@ -10,37 +7,3 @@ def get_home_view():
 
 def get_about_view():
     return render_template('about.html')
-
-
-def get_storages_view(storage_kind=1):
-    return render_template('storage_list.html', storages=get_storages(storage_kind=storage_kind))
-
-
-def get_new_storage_view(storage_kind=1):
-    return render_template('storage_item.html', storage=None, storage_kind=storage_kind)
-
-
-def get_storage_view(storage_id: int):
-    storage=get_storage_by_id(storage_id)
-    return render_template('storage_item.html', storage=storage, storage_kind=storage.kind)
-
-
-def get_storeges_after_deleting_by_id(storage_id: int):
-    delete_storage_by_id(storage_id)
-    return redirect(url_for('storages'))
-
-
-def get_storeges_after_create_update_by_id(storage_id: int, title: str, inn: str='', \
-                                            is_internal: bool=False, is_employee: bool=False, \
-                                            kpp: str='', weight: float=.0, volume: float=.0, \
-                                            kind: int=1):
-    if storage_id:
-        update_storage(id=storage_id, title=title, inn=inn, is_internal=is_internal, \
-            is_employee=is_employee, kpp=kpp,weight=weight, volume=volume, \
-                kind=kind)
-    else:        
-        add_storage(title=title, inn=inn, is_internal=is_internal, \
-            is_employee=is_employee, kpp=kpp,weight=weight, volume=volume, \
-                kind=kind)
-
-    return redirect(url_for('storages'))

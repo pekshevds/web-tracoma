53b3bd35a99809b78aa63b3e077af7d903a184b5
pekshevds <pekshev.ds@gmail.com>
2022-11-03 09:18:23 +0300

    changed parent-class for PointDeleteView and PointListView

--- webapp/views/point/api.py
+++ webapp/views/point/api.py
@@ -4,7 +4,7 @@ from webapp.db.point.fetchers import get_points
 from webapp.db.point.changers import delete_point
 from webapp.views.point.forms import PointForm
 from webapp.views.common import BaseView
-from flask_views.edit import FormView
+from flask_views.base import TemplateView
 
 
 class PointDetailView(BaseView):
@@ -13,18 +13,18 @@ class PointDetailView(BaseView):
     success_url_name = 'point.points'
 
 
-class PointListView(FormView):
+class PointListView(TemplateView):
     template_name = 'point_list.html'
 
     def get(self, *args, **kwargs):
         return render_template('point_list.html', points=get_points())
 
 
-class PointDeleteView(FormView):
-    template_name = 'point_list.html'
+class PointDeleteView(TemplateView):
+    success_url_name = 'point.points'
 
     def get(self, *args, **kwargs):
         id = kwargs.get("id", 0)
         if delete_point(id=id):
-            return render_template('point_list.html', points=get_points())
+            return redirect(url_for(self.success_url_name))
         return render_template("crud_error.html", content='error on mark point for deleting')
